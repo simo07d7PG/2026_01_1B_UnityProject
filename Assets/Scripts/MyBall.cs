8b6ec8f4d4d2f4fdd4348f70b90bc4e8351e9fa4
simo07d7PG <simo07d7.pg@gmail.com>
2026-03-24 10:28:34 +0900

    [Feat] 충돌 시스템

---
+++ Assets/Scripts/MyBall.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MyBall : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name + "와 충돌");
+        if(collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("땅과 충돌");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("트리거 안에 들어옴");
+    }
+}
